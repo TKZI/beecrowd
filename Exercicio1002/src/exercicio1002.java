50a0f34e940a1cee09bfc0bc8e95cae57998cb6e
Tiago <ticruz.13@hotmail.com>
2023-09-22 21:00:21 -0300

    exercises beecrowd

---
+++ Exercicio1002/src/exercicio1002.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class exercicio1002 {
+
+    public static void main(String[] args) {
+
+        final double n = 3.14159;
+        Scanner entrada = new Scanner(System.in);
+        double R = entrada.nextDouble();
+
+        double A = n * Math.pow(R, 2);
+
+        System.out.printf("A=%.4f%n", A);
+
+    }
+
+}
