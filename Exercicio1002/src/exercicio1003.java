8ef8d47a792af7b51c82f30f9d7df323f158a89a
Tiago <ticruz.13@hotmail.com>
2023-09-25 22:18:20 -0300

    exercicio 1003

---
+++ Exercicio1002/src/exercicio1003.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class exercicio1003 {
+
+    public static void main(String[] args) {
+
+        int A, B, SOMA;
+        Scanner entrada = new Scanner(System.in);
+
+        A = entrada.nextInt();
+        B = entrada.nextInt();
+
+        SOMA = A + B;
+
+        System.out.println("SOMA = " + SOMA);
+
+    }
+
+}
