c610a025cd63f05fd768669f9270528235308605
Tiago <ticruz.13@hotmail.com>
2023-12-19 22:08:07 -0300

    atualizando exercicios

---
+++ ExerciciosBeeCrowd/src/exercicio1059.java
@@ -0,0 +1,14 @@
+public class exercicio1059 {
+
+    public static void main(String[] args) {
+
+        Integer numeroPar;
+
+        for (numeroPar = 1; numeroPar <= 100; numeroPar++) {
+            if (numeroPar % 2 == 0) {
+                System.out.println(numeroPar);
+            }
+        }
+    }
+
+}
